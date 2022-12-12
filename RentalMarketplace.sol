bf9bc372a4dd5f59bf4ac76b06d64f2cf120b902
umarrazaaa <umar.raza@invozone.com>
2022-12-12 14:03:14 +0500

    first web3 commit

---
+++ RentalMarketplace.sol
@@ -0,0 +1,10 @@
+// 1 - mintint contract
+// 2 rental marketplace
+
+
+// 721
+// Mint NFT Put on Rent (Startt time and end time for total period rent time)
+// Renter nay ana ha or NFT La jani ha (render time alag ho ga)
+// Time khtam ho jay ga to NFT vapis Lender ko chali jay ge
+// Renter not be able to transfer , only he can see the NFT in his metamask wallet 
+// No KYC - renter needs to pay the price for on the basis of daily days.
